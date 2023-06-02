12bc1aef0788b29c566d306b2857c0e60a1ddf10
Đỗ Tuấn Đạt <datdt@dogoo.com.vn>
2023-06-02 15:55:02 +0700

    update copy to clipboard

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,10 +1,17 @@
-import { UploadOutlined } from "@ant-design/icons";
+import { CopyOutlined, ShareAltOutlined } from "@ant-design/icons";
 import React from "react";
 import { Link } from "react-router-dom";
 import Div from "../Div";
 import "./portfolio.scss";
+import { message } from "antd";
+import copy from "clipboard-copy";
 
 export default function Portfolio({ href, src, title, subtitle, variant }) {
+	const handleCopy = () => {
+		copy(href);
+		message.success("Đã sao chép vào clipboard");
+	};
+
 	return (
 		<Link
 			to={href}
@@ -19,11 +26,23 @@ export default function Portfolio({ href, src, title, subtitle, variant }) {
 					className="cs-portfolio_bg cs-bg"
 					style={{ backgroundImage: `url("${src}")` }}
 				/>
-				<UploadOutlined
-					className="absolute top-[50%] left-[50%] text-[50px]"
-					style={{ transform: "translate(-50%, -50%)", zIndex: 3 }}
-					onClick={(e) => e.preventDefault()}
-				/>
+				<div
+					className="flex items-center justify-center absolute top-2 right-2 cs-portfolio_icon transition-all"
+					style={{ zIndex: 3 }}
+				>
+					<ShareAltOutlined
+						className="text-[25px]"
+						onClick={(e) => e.preventDefault()}
+					/>
+					<span className="mx-3 text-[25px]">/</span>
+					<CopyOutlined
+						className="text-[25px]"
+						onClick={(e) => {
+							e.preventDefault();
+							handleCopy();
+						}}
+					/>
+				</div>
 				<Div className="cs-portfolio_info">
 					<Div className="cs-portfolio_info_bg cs-accent_bg" />
 					<h2 className="cs-portfolio_title">{title}</h2>
