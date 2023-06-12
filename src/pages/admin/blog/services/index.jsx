38bd2a56d73cd108cbfce6b436557316c80aaee2
Do Tuan Dat <dathymax@gmail.com>
2023-06-12 23:43:32 +0700

    update

--- src/pages/admin/blog/services/index.jsx
+++ src/pages/admin/blog/services/index.jsx
@@ -1,14 +1,12 @@
-import { UploadOutlined } from "@ant-design/icons";
-import { Button, Form, Input, message, Spin, Upload } from "antd";
+import { Button, Form, Input, message, Spin } from "antd";
 import React, { useEffect, useRef, useState } from "react";
 import { getDetailBlog, postBlog, updateBlog } from "../../../../apis/blog/api";
 import { getLink } from "../../../../helper/getLink";
 import ReactQuill from "react-quill";
 
 const AdminBlogUpdate = ({ id, closeDrawer, setReloadData }) => {
-	const quillRef = useRef();
+	const quillRef = useRef(null);
 	const [form] = Form.useForm();
-	const [selected, setSelected] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const [content, setContent] = useState("");
 
@@ -16,18 +14,19 @@ const AdminBlogUpdate = ({ id, closeDrawer, setReloadData }) => {
 		const input = document.createElement("input");
 		input.setAttribute("type", "file");
 		input.setAttribute("accept", "image/*");
-		input.onchange = () => {
+		input.click();
+		input.onchange = async () => {
 			const file = input.files[0];
-			getLink(file)
+			const link = await getLink(file)
 				.then((response) => {
-					const quill = quillRef.current.getEditor();
-					console.log("tree", quill);
-					const range = quill.getSelection();
-					quill.insertEmbed(range.index, "image", response);
+					return response;
 				})
 				.catch(() => {});
+			const range = quillRef.current.getEditor().getSelection(true);
+			quillRef.current
+				.getEditor()
+				.insertEmbed(range.index, "image", link);
 		};
-		input.click();
 	};
 
 	const modules = {
@@ -61,21 +60,6 @@ const AdminBlogUpdate = ({ id, closeDrawer, setReloadData }) => {
 		"size",
 		"font",
 	];
-
-	const props = {
-		beforeUpload: (file) => {
-			setLoading(true);
-			getLink(file)
-				.then((response) => {
-					setLoading(false);
-					setSelected(response);
-				})
-				.catch(() => {
-					setLoading(false);
-				});
-		},
-		maxCount: 1,
-	};
 
 	useEffect(() => {
 		if (id) {
@@ -161,74 +145,17 @@ const AdminBlogUpdate = ({ id, closeDrawer, setReloadData }) => {
 						},
 					]}
 				>
-					{/* <Input.TextArea placeholder="Nhập nội dung" /> */}
 					<ReactQuill
 						ref={quillRef}
 						theme="snow"
 						value={content}
-						// onBlur={(value) => setContent(value)}
+						onChange={(content, delta, source, editor) =>
+							setContent(editor.getHTML())
+						}
 						modules={modules}
 						formats={formats}
 					/>
-					{/* <ReactQuill
-						ref={quillRef}
-						value={content}
-						onChange={setContent}
-						modules={{
-							toolbar: {
-								container: [
-									[{ header: [1, 2, 3, 4, false] }],
-									[
-										"bold",
-										"italic",
-										"underline",
-										"strike",
-										"blockquote",
-									],
-									[{ list: "ordered" }, { list: "bullet" }],
-									["link", "image"],
-								],
-								handlers: {
-									image: () => {
-										const input =
-											document.createElement("input");
-										input.setAttribute("type", "file");
-										input.setAttribute("accept", "image/*");
-										input.onchange = () => {
-											const file = input.files[0];
-											getLink(file)
-												.then((response) => {
-													const quill =
-														quillRef.current.getEditor();
-													console.log("tree", quill);
-													const range =
-														quill.getSelection();
-													quill.insertEmbed(
-														range.index,
-														"image",
-														response
-													);
-												})
-												.catch(() => {});
-										};
-										input.click();
-									},
-								},
-							},
-						}}
-					/> */}
 				</Form.Item>
-				{/* <Form.Item
-					name={"img"}
-					label={"Ảnh"}
-					rules={[{ required: true, message: "Chưa chọn ảnh" }]}
-				>
-					<Upload {...props}>
-						<Button icon={<UploadOutlined />}>
-							Click để tải lên
-						</Button>
-					</Upload>
-				</Form.Item> */}
 
 				<Button htmlType="submit">
 					{id ? "Cập nhật" : "Thêm mới"}
